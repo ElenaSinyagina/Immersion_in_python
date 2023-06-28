33db07b49f075952a88c76f1c20450e47f03baeb
Elena <lenaakimova08@rambler.ru>
2023-06-28 15:37:44 +0300

    DZ 2 task 2

--- DZ2/DZ2.py
+++ DZ2/DZ2.py
@@ -13,4 +13,34 @@ while num > 0:
     str = digits[num % 16] + str
     num = num // 16
  
-print("Фактический результат: ", str)
+# print("Результат вычислений: ", str)
+
+
+# Напишите программу, которая принимает две строки
+# вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
+# Для проверки своего кода используйте модуль fractions.
+
+import fractions
+
+numerator1 = int(input("Введите числитель первой дроби "))
+denominator1 = int(input("Введите знаменатель первой дроби "))
+numerator2 = int(input("Введите числитель второй дроби "))
+denominator2 = int(input("Введите знаменатель второй дроби "))
+
+if denominator1 == denominator2:
+    number1 = numerator1 + numerator2 # Числитель дроби в результате сложения
+    number2 = denominator1 # Знаменатель дроби в результате сложения
+else:
+    number1 = (numerator1 * denominator2) + (numerator2 * denominator1) # Числитель дроби в результате сложения
+    number2 = denominator1 * denominator2 # Знаменатель дроби в результате сложения
+
+print(f'Сумма {number1}/{number2}')
+print(f'Произведение {numerator1 * numerator2}/{denominator1 * denominator2}')
+
+firstfraction = fractions.Fraction(numerator1, denominator1)
+secondfraction = fractions.Fraction(numerator2, denominator2)
+result1 = firstfraction + secondfraction
+result2 = firstfraction * secondfraction
+
+print(f'Проверка результатов: сумма {result1}, произведение {result2}')
