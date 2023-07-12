4f96266835446417851fcd34e8555076529a24d0
Elena <lenaakimova08@rambler.ru>
2023-07-12 17:37:13 +0300

    DZ 5 исправления

--- DZ5/DZ5.py
+++ DZ5/DZ5.py
@@ -1,15 +1,15 @@
 # Напишите функцию, которая принимает на вход строку — абсолютный путь до файла. 
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
-# def File_path(path: str) -> tuple:
-#     path, name = path.rsplit('\\', 1)
-#     name, extension = name.split('.')
-#     return path, name, extension
+def File_path(path: str) -> tuple:
+    path, name = path.rsplit('\\', 1)
+    name, extension = name.split('.')
+    return path, name, extension
 
-# print()
-# file_path = r"E:\Geekbrains\Immersion_in_python\text.txt"
-# print(File_path(file_path))
-# print()
+print()
+file_path = r"E:\Geekbrains\Immersion_in_python\text.txt"
+print(File_path(file_path))
+print()
 
 
 # Напишите однострочный генератор словаря, который принимает на вход три списка одинаковой длины: 
@@ -17,11 +17,11 @@
 # с именем в качестве ключа и суммой премии в качестве значения. Сумма рассчитывается 
 # как ставка умноженная на процент премии
 
-# employees = ['Иванов И.И.', 'Петров П.П.', 'Ольгина О.О.', 'Натальина Н.Н.', 'Сидоров С.С.', 'Антонов А.А.']
-# salary_rate = [30000, 35000, 27000, 32000, 48000, 50000]
-# premium = ['10.15', '15.25', '22.35', '10.45', '13.55', '21.65']
-# print({name: rank + (rank * float(prem) / 100) for name, rank, prem
-#        in zip(employees, salary_rate, premium)})
+employees = ['Иванов И.И.', 'Петров П.П.', 'Ольгина О.О.', 'Натальина Н.Н.', 'Сидоров С.С.', 'Антонов А.А.']
+salary_rate = [30000, 35000, 27000, 32000, 48000, 50000]
+premium = ['10.15', '15.25', '22.35', '10.45', '13.55', '21.65']
+print({name: rank + (rank * float(prem) / 100) for name, rank, prem
+       in zip(employees, salary_rate, premium)})
 
 
 # Создайте функцию генератор чисел Фибоначчи (см. Википедию).
